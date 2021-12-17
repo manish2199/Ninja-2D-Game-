c49e09856dc81dbcfa25c6cdaab38fc70672392e
Manish <manishmistry9799@gmail.com>
2021-12-17 19:54:28 +0530

    Created Menu Screen

---
+++ Assets/Scripts/Controllers/MainMenu Controlle/MainMenuController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuController : MonoBehaviour
+{
+    
+  [SerializeField] GameObject levelSelector;
+
+  [SerializeField] GameObject mainMenuPanel;
+  
+
+
+
+
+  public void PlayButton()
+  {
+    levelSelector.SetActive(true);
+    mainMenuPanel.SetActive(false);
+  }
+
+
+
+
+}
