2381260dfef366a387d3348899b1f9e73a18e577
Manish <manishmistry9799@gmail.com>
2022-07-19 20:33:17 +0530

    SetUp the Multiplayer Gameplay Scene

---
+++ Assets/Scripts/MultiPlayer/PhotonConnection/PhotonConnection.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+using WebSocketSharp;
+
+public class PhotonConnection : MonoBehaviourPunCallbacks
+{ 
+    [SerializeField] private GameObject connectedPanel;
+
+    [SerializeField] private GameObject connectionFailPanel;
+
+    [SerializeField] private UIHandler UIHandler;
+
+
+    #region PhotonButtonFunctions
+    private bool IsTextEmpty(string textField)
+    {
+        return textField.IsNullOrEmpty();
+    }
+    
+    public void ConnectToPhoton()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+    
+    public void CreateRoom()
+    {
+        string roomName = UIHandler.CreateRoomNameTxt;
+
+        if(IsTextEmpty(roomName))
+        {
+            return;
+        }
+        
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.MaxPlayers = 2;
+        PhotonNetwork.CreateRoom(roomName,roomOptions, null);
+    }
+    
+    
+    public void JoinRoom()
+    {
+        string joinRoomName = UIHandler.JoinRoomNameTxt;
+
+        if(IsTextEmpty(joinRoomName))
+        {
+            return;
+        }
+        PhotonNetwork.JoinRoom(joinRoomName);
+    }
+    #endregion
+
+
+    #region PhotonCallbacks
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinLobby(TypedLobby.Default);
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        connectionFailPanel.SetActive(true);
+    }
+
+    public override void OnJoinedLobby()
+    {
+        if (connectionFailPanel.activeSelf)
+        {
+            connectionFailPanel.SetActive(false);
+        }
+        connectedPanel.SetActive(true);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("Joined Room");
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log("Failed To Join Room Error : "+message);
+
+    }
+
+    #endregion
+}
---
+++ Assets/Scripts/MultiPlayer/UI Handler/UIHandler.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine;
+
+
+public class UIHandler : MonoBehaviour
+{
+   [SerializeField] private InputField createRoomNameIF;
+   
+   [SerializeField] private InputField joinRoomNameIF;
+
+   public string CreateRoomNameTxt
+   {
+      get
+      {
+         return createRoomNameIF.text;
+      }
+   }
+   
+   public string JoinRoomNameTxt
+   {
+      get
+      {
+         return joinRoomNameIF.text;
+      }
+   }
+}
